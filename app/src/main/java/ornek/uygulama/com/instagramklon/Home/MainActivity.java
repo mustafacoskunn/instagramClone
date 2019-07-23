e221f50b21c3856cea7872bf2edfcc6f2672b2ab
mustafacoskunn <l1621012067@stud.sdu.edu.tr>
2019-07-23 19:23:24 +0300

    Camera Mesaj ve instagram fragmenletleri ayarlandı.

--- app/src/main/java/ornek/uygulama/com/instagramklon/Home/MainActivity.java
+++ app/src/main/java/ornek/uygulama/com/instagramklon/Home/MainActivity.java
@@ -1,9 +1,12 @@
 package ornek.uygulama.com.instagramklon.Home;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TableLayout;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
@@ -20,9 +23,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomNavigationArayuz();
+        viewPagerKurulum();
 
 
     }
+    //3 tane fragment ekle camera home messages
+    private void viewPagerKurulum( ){
+        PagerAdapter adapter=new PagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new CameraFragment()); //fragmentleri ekliyoruz //index 0
+        adapter.addFragment(new HomeFragment()); //index 1
+        adapter.addFragment(new MessagesFragment()); //index 2
+        ViewPager viewPager=findViewById(R.id.container);
+        viewPager.setAdapter(adapter);
+        TabLayout tabLayout=findViewById(R.id.topToolbar);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_insta);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_arrow);
+    }
+
+
     //BottomNavigation ayarları
     private void BottomNavigationArayuz(){
         BottomNavigationViewEx bottomNavigationViewEx=(BottomNavigationViewEx) findViewById(R.id.bottomnavi);// 3. kütüphanenin animasyonunun kullanmak için
