5702de24dd4b446737d1af40af33b1fb5d6f2c0a
mustafacoskunn <l1621012067@stud.sdu.edu.tr>
2019-07-24 20:35:11 +0300

    Hesap ayarları activity tasarimi yapildi.

---
+++ app/src/main/java/ornek/uygulama/com/instagramklon/Home/PagerAdapter.java
@@ -0,0 +1,29 @@
+package ornek.uygulama.com.instagramklon.Home;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.ListFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+//yukarı paneldedeki fragmentler için
+public class PagerAdapter extends FragmentPagerAdapter { //adapterimizi oluşturduk
+    private final List<Fragment> mFragmentList =new ArrayList<>();
+    public PagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+//https://emrealtunbilek.com/android/34-fragment-pager-ve-fragmentstatepager-adapter-android-material-design-egitim-seti/
+    @Override
+    public Fragment getItem(int i) {
+        return mFragmentList.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+    public void addFragment(Fragment fragment){ //yolladıgım fragmentleri listeye ekle
+        mFragmentList.add(fragment);
+    }
+}
---
+++ app/src/main/java/ornek/uygulama/com/instagramklon/Profile/hesapayarlari.java
@@ -0,0 +1,33 @@
+package ornek.uygulama.com.instagramklon.Profile;
+
+import android.content.Intent;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import ornek.uygulama.com.instagramklon.R;
+import ornek.uygulama.com.instagramklon.Utils.BottomNavigationViewHelper;
+
+public class hesapayarlari extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activty_hesapayarlari);
+
+    }
+
+
+
+
+}
